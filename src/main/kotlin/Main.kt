66e371b304634c73f18ed76c037769f9cc2fa895
TajMahal1990 <130366681+TajMahal1990@users.noreply.github.com>
2023-06-19 21:46:58 +0800

    er

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,37 +1,24 @@
-
-
 fun main() {
     println("Приложение подчета коммисии")
-
-    print(commission("МИР", 36, 0))
-    print(" рублей составит коммиссия ")
-    println("Bue")
-
+    print(amountFee(10_000, 75_000, "Maestro"))
+    print(" рублей составит коммисия")
 }
-fun commission(typeCard: String, transfer: Int, previous: Int): Int {
-    return when (typeCard) {
-        "MasterCard", "Maestro" -> when {
-            transfer > 150_000 || transfer + previous > 600_000 -> -1
-            transfer in 301..74999 -> 0
-            else -> (transfer * 0.006f).toInt() + 20 // коммисия составит 0.6% + 20 рублей
-
-        }
-        "Viza", "МИР" -> when {
-            transfer > 150_000 || transfer + previous > 600_000 -> -1
-            (transfer * 0.0075f).toInt() <= 35 -> 35 // минимальная сумма коммисии 35 рублей
-            else -> (transfer * 0.0075f).toInt() // коммисия составит 0.75%
-        }
-        "VK Pay" -> when {
-            transfer > 15_000 || transfer + previous > 150_000 -> -1
-            else -> (transfer * 0.0f).toInt() // нет коммисии
-        }
-
-        else -> {
-            -1
-        }
+val errorDay = -1 // Лимит в день
+val errorMon = -2 // Лимит в месяц
+fun amountFee(
+        amount: Int,
+        amountMonth: Int = 0,
+        TCards: String = "VK Pay"
+): Int = when {
+    TCards == "VK Pay" && amount > 15_000 -> errorDay // Сначала считает вк пэй в лимите
+    TCards == "VK Pay" && amountMonth > (40_000 - amount) -> errorMon
+    TCards != "VK Pay" && amount > 150_000 -> errorDay
+    TCards != "VK Pay" && amountMonth > (600_000 - amount) -> errorMon
+    else -> when { // потом уже переходим на мастеркард, визу и маэстро
+        (TCards == "Mastercard" || TCards == "Maestro") && amountMonth > (75000 - amount) -> amount * 6 / 10 / 100 + 20
+        (TCards == "Visa" || TCards == "МИР") && (amount * 75 / 100 / 100) > 35 -> amount * 75 / 100 / 100
+        (TCards == "Visa" || TCards == "МИР") && (amount * 75 / 100 / 100) < 35 -> 35
+        else -> 0
     }
-
-    }
-
-
+}
 
